51ad2258819128eb2a924e69b21aa590498fe556
kutlu <kutluokan@gmail.com>
2025-01-10 16:44:48 -0600

    1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  }
 };
 
 export default nextConfig;
