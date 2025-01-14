2e9980e27b8c3184eafddafaddd162c74ceaa51e
KutluOkan <kutluokan@gmail.com>
2025-01-14 12:38:45 -0600

    docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com']
   }
 };
 
