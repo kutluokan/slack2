a8b109230ef0d7c9680e75d4388151a0c695158c
KutluOkan <kutluokan@gmail.com>
2025-01-16 13:50:09 -0600

    a

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: 'https://slack2-d1ea1-default-rtdb.firebaseio.com',
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
